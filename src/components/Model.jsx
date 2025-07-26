39d742c5a5b49264d2c4855622ca8482e188946a
Akash Belide <akashbelide8202@gmail.com>
2025-07-26 11:52:05 -0400

    added 3d iphone model to the model section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,5 @@
 import { useGSAP } from '@gsap/react'
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import gsap from 'gsap'
 import ModelView from './ModelView';
 import {yellowImg} from "../utils"
@@ -8,6 +8,8 @@ import { Canvas } from '@react-three/fiber';
 //import { View } from '@react-three/drei';
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
+
 
 
 
@@ -33,7 +35,22 @@ const Model = () => {
 
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+  const tl= gsap.timeline();
 
+  useEffect(()=>{
+    if(size === 'large'){
+      animateWithGsapTimeline(tl,small, smallRotation, '#view1', '#view2', {
+        transform:'translateX(-100%)',
+        duration:2
+      })
+    }
+    if(size === 'small'){
+      animateWithGsapTimeline(tl,large, largeRotation, '#view2', '#view1', {
+        transform:'translateX(0)',
+        duration:2
+      })
+    }
+  },[size])
 
 
 
