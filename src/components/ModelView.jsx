802940d2d2457b87f5cd84440065619a2433cf53
Akash Belide <akashbelide8202@gmail.com>
2025-07-25 22:57:24 -0400

    added model section

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import{View} from "@react-three/drei"
+
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+  
+}
+
+export default ModelView
